734701bd706ff48f7013203996a3eb18967e1bc6
pranav-prm <pranavmutalik17@gmail.com>
2024-09-14 17:22:36 +0530

    Added greet feature

--- GreetingProject/src/main/Greet.java
+++ GreetingProject/src/main/Greet.java
@@ -4,7 +4,12 @@ public class Greet {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-		
+		System.out.println(greetByName("Pranav"));
+	}
+
+	private static String greetByName(String name) {
+		// TODO Auto-generated method stub
+		return "Hello "+name;
 	}
 
 }
